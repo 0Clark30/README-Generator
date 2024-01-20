f8d96a2a679701d941fd2e29748c948686889f31
0Clark30 <austinlaw.clark@outlook.com>
2024-01-19 22:27:00 -0600

    completed mvp

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,94 @@
+const licensesDetails = require('./licensesDetails.js')
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if (license == "None") {
+    return ``;
+  }
+  let result = licensesDetails.filter(licenseDetail => licenseDetail.name == license);
+  return result[0].badge;
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  if (license == "None") {
+    return ``;
+  }
+  let result = licensesDetails.filter(licenseDetail => licenseDetail.name == license);;
+  return result[0].link;
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license == "None") {
+    return ``;
+  }
+  return `## License
+  This application is licensed under [${license}](${renderLicenseLink(license)}) license. Click the link for license rights and limitations.`;
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+
+
+  return `
+  # ${data.title}
+  
+
+  ## Description
+  
+  ${data.description}
+  
+  
+  ## Table of Contents (Optional)
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+ ${data.installation}
+  
+  ## Usage
+  
+  [Deployed](${data.deployedLink})
+  [Github](${data.githubLink})
+  
+  To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+  
+      md
+      ![alt text](assets/images/screenshot.png)
+      
+  
+  ## Credits
+  
+ ${data.credits}
+
+  
+  ## License
+  ${renderLicenseSection(data.license)}
+  
+  
+  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  
+  ## Badges
+
+  ${renderLicenseBadge(data.license)}
+  
+  
+  ## Features
+  
+  ${data.features}
+  
+  ## How to Contribute
+  
+  ${data.contribute}
+  
+  ## Tests
+  ${data.tests}
 `;
 }
 
--- utils/index.js
+++ utils/index.js
@@ -1,58 +1,106 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
 // what will I need to populate the template literal for the readme markdown document
 const questions = [
-    {
-        type: "input",
-        message: "What is your name?",
-        name: "name",
-      },
-      {
-        type: "input",
-        message: "What is your location?",
-        name: "location",
-      },
-      {
-        type: "input",
-        message: "Please type in your bio",
-        name: "bio",
-      },
-      {
-        type: "input",
-        message: "What is your linkedIn URL?",
-        name: "linkedin",
-      },
-      {
-        type: "input",
-        message: "What is your Github URL",
-        name: "githubURL",
-      },
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Project Description?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Please type in your bio",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Github URL?",
+    name: "githubLink",
+  },
+  {
+    type: "input",
+    message: "Deployed URL?",
+    name: "deployedLink",
+  },
+  {
+    type: "input",
+    message: "Credits?",
+    name: "credits",
+  },
+  {
+    type: "list",
+    message: "License?",
+    name: "license",
+    choices: [
+      "None",
+      "Apache license 2.0",
+      "Boost Software License 1.0",
+      'BSD 2-clause "Simplified" license',
+      'BSD 3-clause "New" or "Revised" license',
+      "Creative Commons Zero v1.0 Universal",
+      "Do What The F*ck You Want To Public License",
+      "Eclipse Public License 1.0",
+      "GNU Affero General Public License v3.0",
+      "GNU General Public License v2.0",
+      "GNU General Public License v3.0",
+      "GNU Lesser General Public License v3.0",
+      "MIT",
+      "Mozilla Public License 2.0",
+      "The Unlicense",
+    ],
+  },
+  {
+    type: "input",
+    message: "Features?",
+    name: "features",
+  },
+  {
+    type: "input",
+    message: "How to Contribute?",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "Tests?",
+    name: "tests",
+  },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const {} = questions;
+ 
 
-
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("Success!");
-        }
-      });
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Remember to Edit your inputs");
+    }
+  });
 }
 
 // TODO: Create a function to initialize app
 
 // initialize the template that fills in the responses
 function init() {
-    inquirer
-    .prompt(questions)
-    .then((response)(writeToFile())) 
-}
+    inquirer.prompt(questions)
+      .then((response) => generateMarkdown(response))
+      .then((response2) => {
+        writeToFile("README.md", response2);
+        console.log("README.md file generated successfully.");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
 // Function call to initialize app
 init();
